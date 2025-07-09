7cbe09336f5c44b1513febf9c0da85ad7b938aa5
e314521 <99789999@qq.com>
2025-07-09 18:22:37 +0800

    测试

--- main_ubuntu_android.py
+++ main_ubuntu_android.py
@@ -163,8 +163,7 @@ def main():
     if os.path.exists("../deps.zip"):
         run_command(f"unzip -q ../deps.zip -d {custom_dir}")
 
-    #git_clone_repo()
-    return 0
+
   
 
     ndk_path = os.path.join(os.getcwd(), download_ndk())
